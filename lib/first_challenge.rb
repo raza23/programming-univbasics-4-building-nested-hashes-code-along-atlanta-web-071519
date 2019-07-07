410fe65fa4f653e38152b186b8c538f4cc431844
Raza Tahir Shareef <rtshareef@gmail.com>
2019-07-07 20:15:38 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,9 +1,9 @@
 def first_challenge
   epic_tragedy = {
  :montague => {
-   :patriarch{}
-   :matriarch{}
-   :hero{}
+   :patriarch{},
+   :matriarch{},
+   :hero{},
    :hero_friends{}
  },
  :capulet => {}
