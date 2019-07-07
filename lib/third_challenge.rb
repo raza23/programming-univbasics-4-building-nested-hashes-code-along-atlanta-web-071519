cd528a94d848c5f5afe0511577e09f6da133fc75
Raza Tahir Shareef <rtshareef@gmail.com>
2019-07-07 20:54:14 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,25 +1,35 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => {name:[],
+        age:[]
         #your key/value pairs here
         },
-      :matriarch => {
+      :matriarch => {name:[],
+        age:[]
+        
         #your key/value pairs here
         },
-      :hero => {
+      :hero => {name:[],
+        age:[],
+        status:[]
         #your key/value pairs here
         },
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
+      :patriarch => {name:[],
+        age:[]
         #your key/value pairs here
         },
       :matriarch => {
+        name:[],
+          age:[]
         #your key/value pairs here
         },
-      :heroine => {
+      :heroine => {name:[],
+        age:[],
+        status:[]
         #your key/value pairs here
         },
       :heroine_friends => []
