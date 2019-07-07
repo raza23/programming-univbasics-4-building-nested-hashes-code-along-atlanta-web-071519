a28161713447c4d8aa88245d26ddaafb23e0ad59
Raza Tahir Shareef <rtshareef@gmail.com>
2019-07-07 20:38:43 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,10 +7,10 @@ def second_challenge
       hero_friends:{}#your key/value pairs here!
       },
     :capulet => {
-      patriarch:{{}},
-      matriarch:{{}},
-      heroine:{{}},
-      heroine_friends:{{}}
+      patriarch:{},
+      matriarch:{},
+      heroine:{},
+      heroine_friends:{}
       #your key/value pairs here!
       }}
 
