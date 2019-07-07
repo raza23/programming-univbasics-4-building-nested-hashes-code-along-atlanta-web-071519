e8ee4322ef9b369ea599949a1ea568e6e76a2983
Raza Tahir Shareef <rtshareef@gmail.com>
2019-07-07 20:54:17 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,7 +7,7 @@ def third_challenge
         },
       :matriarch => {name:[],
         age:[]
-        
+
         #your key/value pairs here
         },
       :hero => {name:[],
@@ -16,7 +16,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {name:[],
         age:[]
@@ -36,5 +36,5 @@ def third_challenge
    }
   }
 
-  
+
 end
