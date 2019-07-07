28217af49853b8651b4c50e70875733f6a2372bb
Raza Tahir Shareef <rtshareef@gmail.com>
2019-07-07 21:05:53 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -16,8 +16,8 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => [
-        { name:"Benvolio",age:"17",attitude:"worried"},
-        { name:"Mercutio",age:"18",attitude:"hot-headed"}]
+        { name:"Benvolio",age:"17",attitude:"worried"}],
+        [{ name:"Mercutio",age:"18",attitude:"hot-headed"}]
 
    },
    :capulet => {
