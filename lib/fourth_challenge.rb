9043f467be3fac048313e561d2b27fb03379bc3b
Raza Tahir Shareef <rtshareef@gmail.com>
2019-07-07 21:13:07 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -13,6 +13,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
+        { name:"Benvolio",age:"17",attitude:"worried"},
+        { name:"Mercutio",age:"18",attitude:"hot-headed"}
         #your hashes here!
       ]
    }
