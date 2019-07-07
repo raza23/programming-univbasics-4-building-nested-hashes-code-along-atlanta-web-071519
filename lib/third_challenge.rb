c9ac7fb839651c43442b3914f67cb80f811dfc06
Raza Tahir Shareef <rtshareef@gmail.com>
2019-07-07 21:10:50 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,14 +15,16 @@ def third_challenge
         status:"alive"
         #your key/value pairs here
         },
-      :hero_friends => [
-        { name:"Benvolio",
-          age:"17",
-          attitude:"worried"
+        :hero_friends => [
+        {
+           :name => "Benvolio",
+           :age => "17",
+           :attitude => "worried"
         },
-        { name:"Mercutio",
-          age:"18",
-          attitude:"hot-headed"
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
         }
       ]
 
